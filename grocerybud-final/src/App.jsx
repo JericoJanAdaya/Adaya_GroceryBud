015be587306945ef53f1422dbe2c28c125ac8afd
Penguin Mastermind <jerico.adaya@usjr.edu.ph>
2022-11-07 22:06:45 +0800

    implement adding item to list

--- grocerybud-final/src/App.jsx
+++ grocerybud-final/src/App.jsx
@@ -1,22 +1,48 @@
 import React, { useState, useEffect } from 'react';
+import List from './List';
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    return (list = JSON.parse(localStorage.getItem('list')));
+  } else {
+    return [];
+  }
+};
 function App() {
   const [item, setItem] = useState('');
+  const [list, setList] = useState(getLocalStorage());
+  const grocerySubmit = (e) => {
+    e.preventDefault();
+    const newItem = { id: new Date().getTime().toString(), title: item };
+    setList([...list, newItem]);
+    setItem('');    
+  };
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list]);
   return (
     <section className='section-center'>
       <h3>grocery bud</h3>
       <div className='underline'></div>
-      <div className='form-control'>
-        <input
-          type='text'
-          className='grocery'
-          placeholder='e.g. milk'
-          value={item}
-          onChange={(e) => setItem(e.target.value)}
-        />
-        <button type='submit' className='submit-btn'>
-          submit
-        </button>
-      </div>  
+      <form className='grocery-form' onSubmit={grocerySubmit}>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. milk'
+            value={item}
+            onChange={(e) => setItem(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            submit
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} />          
+        </div>
+      )}       
     </section>    
   )
 }
---
+++ grocerybud-final/src/List.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+const List = ({ items }) => {
+  return (
+    <div className='grocery-list'>
+      {items.map((item) => {
+        const { index, title } = item;
+        return (
+          <article className='grocery-item' key={index}>
+            <p className='title'>{title}</p>            
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default List;
