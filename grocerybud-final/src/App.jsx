a97ba7ca20841da75192f144b6be12e719bb869e
Penguin Mastermind <jerico.adaya@usjr.edu.ph>
2022-11-07 21:35:28 +0800

    implement text field and submit button

--- grocerybud-final/src/App.jsx
+++ grocerybud-final/src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
-import './index.css'
-
+import React, { useState, useEffect } from 'react';
 function App() {
+  const [item, setItem] = useState('');
   return (
-    <h2>Grocery Bud Setup</h2>
+    <section className='section-center'>
+      <h3>grocery bud</h3>
+      <div className='underline'></div>
+      <div className='form-control'>
+        <input
+          type='text'
+          className='grocery'
+          placeholder='e.g. milk'
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <button type='submit' className='submit-btn'>
+          submit
+        </button>
+      </div>  
+    </section>    
   )
 }
 
-export default App
+export default App;
